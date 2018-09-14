ae2bb57c93f8b0a4cc0ced139968409795403540
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-09-15 00:08:42 +0300

    fixed runServer ignoring arguments

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL ||  'mongodb://JustinNietz1:trumpet1@ds115762.mlab.com:15762/music-form-login-assignments';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||  'mongod://localhost/music-form-login-assignments';
+require('dotenv').config();
+
+exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
